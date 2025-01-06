f9ce88fd5efa6bf6cbcaaa5880c3b440119c4130
chouchouji <1305974212@qq.com>
2025-01-06 11:39:26 +0800

    fix: fix multiple annotation parsing error

--- src/utils.ts
+++ src/utils.ts
@@ -6,16 +6,9 @@ type Traverse = { default: typeof _traverse };
 
 const traverse = (_traverse as unknown as Traverse).default;
 
-const magicComments = [
-  {
-    comment: 'vitePrefetch: true',
-    rel: 'prefetch',
-  },
-  {
-    comment: 'vitePreload: true',
-    rel: 'preload',
-  },
-];
+const magicCommentMap = new Map<string, string>()
+  .set('vitePrefetch: true', 'prefetch')
+  .set('vitePreload: true', 'preload');
 
 export function extractPreloadModules(code: string) {
   const dynamicImportModules: PreloadModule[] = [];
@@ -25,7 +18,7 @@ export function extractPreloadModules(code: string) {
   });
 
   traverse(ast, {
-    CallExpression: path => {
+    CallExpression: (path) => {
       if (path.node.callee.type !== 'Import' || !path.node.arguments.length) {
         return;
       }
@@ -34,12 +27,20 @@ export function extractPreloadModules(code: string) {
         type,
         leadingComments,
         value = '',
-      } = path.node.arguments[0] as (typeof path.node.arguments)[0] & { value: string };
+      } = path.node.arguments[0] as (typeof path.node.arguments)[0] & {
+        value: string;
+      };
 
-      if (type === 'StringLiteral' && leadingComments?.length && leadingComments[0].type === 'CommentBlock') {
-        const magicComment = magicComments.find(({ comment }) => comment === leadingComments[0].value.trim());
-        magicComment && dynamicImportModules.push({ moduleId: value, rel: magicComment.rel });
+      if (type !== 'StringLiteral' || !leadingComments?.length) {
+        return;
       }
+
+      leadingComments
+        .filter((comment) => comment.type === 'CommentBlock')
+        .forEach((comment) => {
+          const rel = magicCommentMap.get(comment.value.trim());
+          rel && dynamicImportModules.push({ moduleId: value, rel });
+        });
     },
   });
 
