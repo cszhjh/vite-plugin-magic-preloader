333acc35fddf01e3d8d311cec2277d753c8f10b6
chouchouji <70570907+chouchouji@users.noreply.github.com>
2025-01-06 15:55:17 +0800

    test: add examples (#10)
    * test: add examples
    
    * docs: add readme
    
    * chore: add license
    
    * docs: optimize example code
    
    ---------
    
    Co-authored-by: cszhjh <cszhjh@gmail.com>

---
+++ packages/examples/src/views/about/about-router.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { Button as VarButton } from '@varlet/ui'
+import '@varlet/ui/es/button/style/index.mjs'
+
+import { normalizeToArray } from '@/utils/utils-array'
+import { bigCamelize } from '@/utils/utils-string'
+
+import TestComponent from './test'
+
+defineOptions({
+  name: 'AboutRouter'
+})
+
+const router = useRouter()
+
+function navigateToHomePage() {
+  router.push({
+    path: '/'
+  })
+}
+
+onMounted(() => {
+  console.log('about')
+  console.log(normalizeToArray('123'))
+  console.log(bigCamelize('chouchouji'))
+})
+</script>
+
+<template>
+  <h1>about</h1>
+  <VarButton type="primary" style="width: 150px" @click="navigateToHomePage"
+    >go to home page</VarButton
+  >
+  <TestComponent />
+</template>
